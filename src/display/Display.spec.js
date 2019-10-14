b5797251dcab42c0d99401a2057e614c8fe28508
LenWinkler <lenwinkler86@gmail.com>
2019-10-14 18:44:29 -0400

    wrote test to check default gate status

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,16 +1,17 @@
 // Test away!
 
 import React from 'react';
-import { render, getByText } from 'react-testing-library';
+import { render } from 'react-testing-library';
 
 import Display from './Display';
 
 test('Display renders', () => {
     render(<Display />);
-})
+});
 
-test('starts out locked', () => {
-    render(<Display />);
-    const locked = getByText('lockedClass');
-    expect(locked).toBe('Unlocked');
-})
+test('displays are "unlocked" and "open" by default', () => {
+    const { getByText } = render(<Display />);
+
+    getByText(/open/i);
+    getByText(/unlocked/i);
+});
