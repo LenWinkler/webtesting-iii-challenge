ddd972696132ba3094bfe56beae6fa810e130768
LenWinkler <lenwinkler86@gmail.com>
2019-10-14 16:58:41 -0400

    wrote first test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+
+import React from 'react';
+import { render, getByText } from 'react-testing-library';
+
+import Display from './Display';
+
+test('Display renders', () => {
+    render(<Display />);
+})
+
+test('starts out locked', () => {
+    render(<Display />);
+    const locked = getByText('lockedClass');
+    expect(locked).toBe('Unlocked');
+})
